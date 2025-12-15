0ee788897837739afe3cb5e00d4bb9574452a7e4
Matthew Pratt <matt@sentrl.com.au>
2025-12-16 01:51:20 +0930

    Revert "Update vite.config.ts"
    This reverts commit 9de7cb54359272ae4bc716fb46184dbe24046179.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>SENTRL</title>
-  </head>
-  <body>
-    <div id="root"></div>
-    <script type="module" src="/index.tsx"></script>
-  </body>
-</html>
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  base: '/',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    },
+  },
+});
