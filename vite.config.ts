a718db8e6023a5eeddcd106bf356d3365974a9bc
Matthew Pratt <matt@sentrl.com.au>
2025-12-16 01:51:20 +0930

    Revert "Update vite.config.ts"
    This reverts commit de74438bda203d43013c5d0ea4e9afe4aada286a.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,25 @@
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  base: '/',
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '.'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '');
+  return {
+    base: '/',                     // REQUIRED FOR NETLIFY
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
     },
-  },
+    plugins: [react()],
+    define: {
+      'process.env': {},           // REQUIRED TO PREVENT RUNTIME CRASH
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      },
+    },
+  };
 });
